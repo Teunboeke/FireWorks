ab0f415611b1ef0e53ae6873ad2b6662c97e4cb5
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-18 21:23:43 +0100

    Update FireWorks.php

--- src/Teunboeke/JoinFireWorks/Item/FireWorks.php
+++ src/Teunboeke/JoinFireWorks/Item/FireWorks.php
@@ -14,7 +14,7 @@ use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\nbt\tag\ListTag;
 use pocketmine\Player;
 use pocketmine\utils\Random;
-use Teunboeke\fireworks\entity\FireworksRocket;
+use Teunboeke\FireworksRocket\entity\FireworksRocket;
 
 class Fireworks extends item{
 
