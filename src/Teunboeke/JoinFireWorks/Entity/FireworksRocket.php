b02b0b4839f297d27ff0638321e859c4589c9d0c
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-18 17:20:57 +0100

    Update FireworksRocket.php

--- src/Teunboeke/JoinFireWorks/Entity/FireworksRocket.php
+++ src/Teunboeke/JoinFireWorks/Entity/FireworksRocket.php
@@ -1,3 +1,16 @@
 <?php
 
 namespace Teunboeke\JoinFireWorks\Entity
+
+use pocketmine\entity\Entity; 
+use pocketmine\entity\projectile\Projectile;
+use pocketmine\level\Level;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\network\mcpe\protocol\ActorEventPacket;
+use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
+use pocketmine\Player;
+use pocketmine\utils\Random;
+use xenialdan\fireworks\item\Fireworks;
+
+class FireworksRocket extends Projectile{
+  	const NETWORK_ID = self::FIREWORKS_ROCKET;
