3ef19ffafcd34918b6ae3d30e4914dd58f9988c3
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-18 17:39:55 +0100

    Update FireWorks.php

--- src/Teunboeke/JoinFireWorks/Item/FireWorks.php
+++ src/Teunboeke/JoinFireWorks/Item/FireWorks.php
@@ -3,3 +3,17 @@
 declare(strict_types=1);
 
 namespace Teunboeke\JoinFireWorks\Item
+
+use pocketmine\block\Block;
+use pocketmine\entity\Entity;
+use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\nbt\NBT;
+use pocketmine\nbt\tag\ByteTag;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\Player;
+use pocketmine\utils\Random;
+use xenialdan\fireworks\entity\FireworksRocket;
+
+class Fireworks extends item{
