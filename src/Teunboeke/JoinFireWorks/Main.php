9dabbda93984473533d6a49bfed76bf62223400c
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-18 15:54:45 +0100

    Update Main.php

--- src/Teunboeke/JoinFireWorks/Main.php
+++ src/Teunboeke/JoinFireWorks/Main.php
@@ -1,3 +1,12 @@
 <?php
 
 namespace Teunboeke\JoinFireWorks;
+
+use pocketmine\entity\Entity;
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
+use pocketmine\plugin\PluginBase;
+use xenialdan\fireworks\entity\FireworksRocket;
+use xenialdan\fireworks\item\Fireworks;
+
+class Main extends PluginBase}
